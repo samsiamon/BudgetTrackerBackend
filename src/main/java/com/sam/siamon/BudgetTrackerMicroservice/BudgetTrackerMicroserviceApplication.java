073e39e729785507f5b42fdf4a694cbfcaa2d040
Sam Siamon <sam.siamon@neudesic.com>
2023-04-17 15:47:18 -0700

    initial commit

---
+++ src/main/java/com/sam/siamon/BudgetTrackerMicroservice/BudgetTrackerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sam.siamon.BudgetTrackerMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/siamon/BudgetTrackerMicroservice/BudgetTrackerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.siamon.BudgetTrackerMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
